977bfca9171fb1314c852fa1cd0fb545f8504a69
tvolpert <timothyvolpert@gmail.com>
2018-08-24 14:12:18 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package AmilyFalbum
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+	</footer><!-- #colophon -->
+	<a href="#bottom-drawer" id="drawerpull">Add +</button><section id="bottom-drawer"><a href="#" class="escape"></a><aside id="post-form"><?php echo do_shortcode('[wpuf_form id="31"]') ?></aside></section>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
