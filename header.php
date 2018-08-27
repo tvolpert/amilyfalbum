1f0715f1b3e3d50b00fac638fe5946a7a5782c56
tvolpert <timothyvolpert@gmail.com>
2018-08-27 13:51:27 -0500

    basic layout tweaks

--- header.php
+++ header.php
@@ -28,15 +28,12 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+	
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+		
+		
 			$amilyfalbum_description = get_bloginfo( 'description', 'display' );
 			if ( $amilyfalbum_description || is_customize_preview() ) :
 				?>
