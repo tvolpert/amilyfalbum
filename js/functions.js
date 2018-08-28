d32677899047c0a1af1aa368b9fabb2b0e4834db
tvolpert <timothyvolpert@gmail.com>
2018-08-28 14:14:49 -0500

    got dark mode actually working and persisting

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 (function($) { 
 //wrap all code in this anonymous function to enable use of the $ for jQuery
 
-	console.log('hallo woild');
+
 	//homemade lightbox for opening images
 	$('[rel="lightbox"]').click(function( event ) {
 		event.preventDefault();
@@ -13,12 +13,27 @@
 		} );
 	} );
 
-	//toggle dark.light mode
-	$('#dark-mode').click(function() {
-			$('body').toggleClass('dark-mode');
+	//toggle dark/light mode
+	$('#dark-mode').click( 	function() {
+	
+		if ( document.cookie.match(/theme=dark/i) ) {
+			document.cookie = 'theme=light'
+		} else {
+			document.cookie = 'theme=dark'
+		}
+				$('#page').toggleClass('dark-mode');
+				console.log('roger that');
 	});
 	
-
+	function setThemeFromCookie() {
+		if ( document.cookie.match(/theme=dark/i) ) {
+			$('#page').addClass('dark-mode');
+		} else {
+	
+			$('#page').removeClass('dark-mode')
+		}
+	};
+	setThemeFromCookie();
 	
 	
 	
