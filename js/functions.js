0a0429ccf17d4a401a14a627fb96c2a54ec93f59
tvolpert <timothyvolpert@gmail.com>
2018-08-28 13:07:03 -0500

    Revert "tryna make dark mode persist"
    This reverts commit 8a07f896989619989b61254f8161462254cdc428. but changes "darkMode" to "dark-mode"  for convenience

--- js/functions.js
+++ js/functions.js
@@ -12,31 +12,13 @@
 		$(this).detach();
 		} );
 	} );
-<<<<<<< HEAD
+
 	//toggle dark.light mode
-	
-		function togglePageContentLightDark() {
-		  var body = document.getElementById('body')
-		  var currentClass = body.className
-		  var newClass = body.className == 'dark-mode' ? 'light-mode' : 'dark-mode';
-		  body.className = newClass;
-
-		  document.cookie = 'theme=' + (newClass == 'light-mode' ? 'light' : 'dark');
-	
-		
-			function setThemeFromCookie() {
-			  var body = document.getElementById('body')
-			  $('body').addClass(isDarkThemeSelected() ? 'dark-mode' : 'light-mode');
-			};	  
-			console.log('Cookies are now: ' + document.cookie);
-				$('#dark-mode').click(setThemeFromCookie());	
-		
-		};
+	$('#dark-mode').click(function() {
+			$('body').toggleClass('dark-mode');
+	});
 	
 
-	
-=======
->>>>>>> parent of 59c16c1... added Dark mode
 	
 	
 	
